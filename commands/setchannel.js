d54ec7c4a141e87f293731a2e186a1f6b97f51e7
@nukelet <vinicius241201@gmail.com>
2020-11-11 03:19:02 -0300

    Fix last commit

---
+++ commands/setchannel.js
@@ -0,0 +1,9 @@
+module.exports =
+{
+    name: 'setchannel',
+    description: `Set in which channels the bot can be used (defaults to all channels). Syntax: ${prefix}setchannel {channel-id}.`,
+    execute(message, args)
+    {
+        
+    }
+}
--- main.js
+++ main.js
@@ -1,9 +1,52 @@
-console.log("Sup fuckers");
+const { version, prefix } = require('./config.json');
 
-var car =
+console.log(`CavaloBot ${version}`);
+console.log('Starting...');
+
+require('dotenv').config();
+const TOKEN = process.env.DISCORD_TOKEN;
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// read all commands
+client.commands = new Discord.Collection();
+const fs = require('fs');
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles)
 {
-    model: "cavalo",
-    price: 1999.99
-};
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
-console.log(car);
+client.login(TOKEN);
+
+client.once('ready', () =>
+{
+    console.log('Login successful!');
+});
+
+const allowedChannels = new Discord.Collection();
+
+client.on('message', message =>
+{
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split('/\s+/');
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+
+    try
+    {
+        client.commands.get(command).execute(message, args);
+    }
+
+    catch (error)
+    {
+        console.log(error);
+        message.reply(`Unknown error while executing command "${command}".`);
+    }
+
+});
