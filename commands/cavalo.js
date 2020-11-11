b83a5a5846bf3f553501ad4b9ff03a8e9fc4f006
@nukelet <vinicius241201@gmail.com>
2020-11-11 03:06:59 -0300

    Remove dependency files, set up basic bot client
    - Dependencies installed: discord.js, dotenv
    - Add dynamic command parsing
    - Add "!cavalo" command

---
+++ commands/cavalo.js
@@ -0,0 +1,11 @@
+module.exports =
+{
+    name: 'cavalo',
+    description: 'cavalo',
+
+    // eslint-disable-next-line no-unused-vars
+    execute(message, args)
+    {
+        message.channel.send('cavalo :racehorse:');
+    },
+};
